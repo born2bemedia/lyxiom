13c47b86ac028ca3a1b9b3b232fe04ea44b9f307
SergWebAcceptance <serg.web@acceptance.ltd>
2024-11-29 11:08:29 +0200

    Added header

--- src/app/components/Header.jsx
+++ src/app/components/Header.jsx
@@ -1,9 +1,128 @@
-import React from 'react'
+"use client";
+import React, { useEffect, useState } from "react";
+import "@/styles/header.scss";
+import Link from "next/link";
 
 const Header = () => {
-  return (
-    <div>Header</div>
-  )
-}
+  const [menuOpened, setMenuOpened] = useState(false);
+  const [submenuOpen, setSubmenuOpen] = useState({});
+  const [isSmallScreen, setIsSmallScreen] = useState(false);
+  const [buttonActive, setButtonActive] = useState(false);
 
-export default Header
+  const [hoveredItem, setHoveredItem] = useState(null);
+
+  const menuOpen = () => {
+    setMenuOpened((prev) => !prev);
+    setButtonActive((prev) => !prev);
+    document.body.classList.toggle("no-scroll", !menuOpened);
+  };
+
+  const closeMenu = () => {
+    setMenuOpened(false);
+    setButtonActive(false);
+    document.body.classList.remove("no-scroll");
+  };
+
+  const handleMouseEnter = (index) => {
+    setHoveredItem(index);
+  };
+
+  const handleMouseLeave = () => {
+    setHoveredItem(null);
+  };
+
+  useEffect(() => {
+    const handleResize = () => {
+      setIsSmallScreen(window.innerWidth <= 992);
+    };
+
+    handleResize();
+    window.addEventListener("resize", handleResize);
+
+    return () => {
+      window.removeEventListener("resize", handleResize);
+    };
+  }, []);
+
+  return (
+    <header className="header">
+      <div className="header__container _container">
+        <div className="header__body">
+          <div className="header__col-01">
+            <Link href="/" className="header__logo" onClick={closeMenu}>
+              <img src="/images/logo.svg" alt="logo" />
+            </Link>
+          </div>
+          <div className="header__col-02">
+            <button onClick={menuOpen} className={`header__menu-btn ${menuOpened ? "_active" : ""}`}>
+              {!menuOpened ? (
+                <img src="/images/menu-close.svg" alt="menu-burger" />
+              ) : (
+                <img alt="logo" src="/images/menu-open.svg" />
+              )}
+            </button>
+          </div>
+
+          <div className={`header__menu ${menuOpened ? "_active" : ""}`}>
+            <nav className="header__nav _container">
+              <ul className="header__list">
+                <li
+                  className={`header__item-menu _submenu ${submenuOpen["services"] || hoveredItem === "services" ? "_open" : ""}`}
+                  onMouseEnter={() => handleMouseEnter("services")}
+                  onMouseLeave={handleMouseLeave}
+                >
+                  <div className="wrapper">
+                    <Link href="/services" onClick={closeMenu}>Our Services</Link>
+                    <button type="button" className="btn">
+                      {!submenuOpen["services"] ? (
+                        <img src="/images/menu-right.svg" alt="expand" />
+                      ) : (
+                        <img src="/images/menu-left.svg" alt="collapse" />
+                      )}
+                    </button>
+                  </div>
+                  <ul
+                    className={`header__submenu ${submenuOpen["services"] || hoveredItem === "services" ? "_active" : ""}`}
+                    style={{
+                      maxHeight: submenuOpen["services"] || hoveredItem === "services" ? "1000px" : "0",
+                      overflow: "hidden",
+                      transition: "all 0.3s ease 0s",
+                      paddingTop: submenuOpen["services"] || hoveredItem === "services" ? "10px" : "0",
+                    }}
+                  >
+                    <li className="header__subitem">
+                      <Link href="#" onClick={closeMenu}>Brand Design</Link>
+                    </li>
+                    <li className="header__subitem">
+                      <Link href="#" onClick={closeMenu}>Product, Service, Experience Design</Link>
+                    </li>
+                    <li className="header__subitem">
+                      <Link href="#" onClick={closeMenu}>SMM</Link>
+                    </li>
+                    <li className="header__subitem">
+                      <Link href="#" onClick={closeMenu}>SEO</Link>
+                    </li>
+                    <li className="header__subitem">
+                      <Link href="#" onClick={closeMenu}>Performance (Advertising)</Link>
+                    </li>
+                    <li className="header__subitem">
+                      <Link href="#" onClick={closeMenu}>Brand Scaling</Link>
+                    </li>
+                  </ul>
+                </li>
+
+                <li className="header__item-menu"><Link href="#" onClick={closeMenu}>Complex Solutions</Link></li>
+                <li className="header__item-menu"><Link href="#" onClick={closeMenu}>Introducing Lyxiom</Link></li>
+                <li className="header__item-menu"><Link href="#" onClick={closeMenu}>Your Way</Link></li>
+                <li className="header__item-menu"><Link href="#" onClick={closeMenu}>Branding Courses</Link></li>
+                <li className="header__item-menu"><Link href="#" onClick={closeMenu}>Contact Us</Link></li>
+              </ul>
+            </nav>
+          </div>
+        </div>
+      </div>
+    </header>
+  );
+};
+
+export default Header;
